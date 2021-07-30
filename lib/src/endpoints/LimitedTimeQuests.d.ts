a7af5d0c1bde569ae8dbc1880d902ab448f98ca3
NicolasT <nicolastem@hotmail.fr>
2021-07-31 01:26:49 +0200

    add claim ltq reward

--- lib/src/endpoints/LimitedTimeQuests.d.ts
+++ lib/src/endpoints/LimitedTimeQuests.d.ts
@@ -1,9 +1,10 @@
 import { GoatResource } from '../GoatResource';
-import { LTQStatus } from '../../types';
+import { LTQ_TYPES, LTQStatus } from '../../types';
 export declare class LimitedTimeQuests extends GoatResource {
     loginQuest(): Promise<LTQStatus>;
     gemsQuest(): Promise<LTQStatus>;
     marriageQuest(): Promise<LTQStatus>;
-    randomVisits(): Promise<LTQStatus>;
-    energyDraught(): Promise<LTQStatus>;
+    randomVisitsQuest(): Promise<LTQStatus>;
+    energyDraughtQuest(): Promise<LTQStatus>;
+    claimRewards(type: LTQ_TYPES): Promise<void>;
 }
--- src/endpoints/LimitedTimeQuests.ts
+++ src/endpoints/LimitedTimeQuests.ts
@@ -1,5 +1,5 @@
 import { GoatResource } from '../GoatResource'
-import { LTQStatus } from '../../types'
+import { LTQ_TYPES, LTQStatus } from '../../types'
 
 export class LimitedTimeQuests extends GoatResource {
 	async loginQuest(): Promise<LTQStatus> {
@@ -14,12 +14,16 @@ export class LimitedTimeQuests extends GoatResource {
 		const data = await this.request({ 'huodong':{ 'hd210Info':[] },'rsn':'7ycclsglpvg' })
 		return data.a.xshuodong.lianyin
 	}
-	async randomVisits(): Promise<LTQStatus> {
+	async randomVisitsQuest(): Promise<LTQStatus> {
 		const data = await this.request({ 'huodong':{ 'hd1045Info':[] },'rsn':'1tbbqeueiu' })
 		return data.a.xshuodong.sjxo
 	}
-	async energyDraught(): Promise<LTQStatus> {
+	async energyDraughtQuest(): Promise<LTQStatus> {
 		const data = await this.request({ 'huodong':{ 'hd220Info':[] },'rsn':'3zeehfwzkhe' })
 		return data.a.xshuodong.huolidan
+	}
+	async claimRewards(type: LTQ_TYPES): Promise<void> {
+		const data = JSON.parse(`{ "huodong":{ "hd${type}Rwd":[] },"rsn":"6xwwbpglbgg" }`)
+		await this.request(data)
 	}
 }
