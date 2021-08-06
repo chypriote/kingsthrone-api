c734398904c025829724ba8618d64b7a412eaa2a
NicolasT <nicolastem@hotmail.fr>
2021-08-06 13:03:25 +0200

    return island status

--- lib/src/endpoints/Events/MysteriousIsland.d.ts
+++ lib/src/endpoints/Events/MysteriousIsland.d.ts
@@ -1,8 +1,8 @@
 import { GoatResource } from '../../GoatResource';
-import { MysteriousIslandStatus, MysteriousIslandFight } from '../../../types/Events/MysteriousIsland';
+import { MysteriousIslandStatus, MysteriousIslandFight } from '../../../types';
 export declare class MysteriousIsland extends GoatResource {
     eventInfos(): Promise<MysteriousIslandStatus>;
-    selectIsland(id: number): Promise<void>;
+    selectIsland(id: number): Promise<MysteriousIslandStatus>;
     selectHero(id: number): Promise<void>;
     fight(id: number): Promise<MysteriousIslandFight>;
     getLevelReward(id: number): Promise<void>;
--- src/endpoints/Events/MysteriousIsland.ts
+++ src/endpoints/Events/MysteriousIsland.ts
@@ -1,13 +1,14 @@
 import { GoatResource } from '../../GoatResource'
-import { MysteriousIslandStatus, MysteriousIslandFight } from '../../../types/Events/MysteriousIsland'
+import { MysteriousIslandStatus, MysteriousIslandFight } from '../../../types'
 
 export class MysteriousIsland extends GoatResource {
 	async eventInfos(): Promise<MysteriousIslandStatus> {
 		const data = await this.request({ 'huodong':{ 'hd1089Info':[] } })
 		return data.a.floatingIsland
 	}
-	async selectIsland(id: number): Promise<void> {
-		await this.request({ 'huodong':{ 'hd1089Select':{ 'floorIndex':id } } })
+	async selectIsland(id: number): Promise<MysteriousIslandStatus> {
+		const data = await this.request({ 'huodong':{ 'hd1089Select':{ 'floorIndex':id } } })
+		return data.a.floatingIsland
 	}
 	async selectHero(id: number): Promise<void> {
 		await this.request({ 'huodong':{ 'hd1089SelectHero':{ 'heroId':id } } })
